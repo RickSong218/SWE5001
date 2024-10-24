ae43ce3cc2cf0c5b4374841245118c5e944e60e2
Apeir <89534239+Apeir@users.noreply.github.com>
2024-10-24 16:39:18 +0800

    mail&bot

--- bot-api/src/main/java/com/ssms/bot/dto/AlertRemovedShiftRequest.java
+++ bot-api/src/main/java/com/ssms/bot/dto/AlertRemovedShiftRequest.java
@@ -1,19 +1,22 @@
-package com.SSMS.company.dto;
+package com.ssms.bot.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.ssms.company.dto.ShiftDto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class DirectoryEntryRequest {
-    @NotBlank
-    private String companyId;
+public class AlertRemovedShiftRequest {
     @NotBlank
     private String userId;
+    @NotNull
+    private ShiftDto oldShift;
 }
--- bot-api/src/main/java/com/ssms/bot/dto/AlertRemovedShiftsRequest.java
+++ bot-api/src/main/java/com/ssms/bot/dto/AlertRemovedShiftsRequest.java
@@ -1,22 +1,22 @@
-package com.SSMS.company.dto;
+package com.ssms.bot.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.ssms.company.dto.ShiftDto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class WorkerDto {
-    @NotBlank
-    private String companyId;
-    @NotBlank
-    private String teamId;
+public class AlertRemovedShiftsRequest {
     @NotBlank
     private String userId;
+    @Builder.Default
+    private List<ShiftDto> oldShifts = new ArrayList<>();
 }
